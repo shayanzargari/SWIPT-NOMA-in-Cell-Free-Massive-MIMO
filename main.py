fcce42e04c7aff27350675c76589af28c329e6f1
Shayan Zargari <104786563+shayanzargari@users.noreply.github.com>
2026-05-19 01:29:54 -0400

    Use paper-level simulation engine in main runner

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import argparse
 
-from src.config import SimulationConfig
+from src.paper_params import PAPER_PARAMS
+from src.paper_simulation import PaperSimulation
 from src.plotting import plot_capacity, plot_system_model
-from src.simulation import MonteCarloSimulation
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--mc', type=int, default=200)
+parser = argparse.ArgumentParser(
+    description='Run the full SWIPT-NOMA cell-free massive MIMO reproduction pipeline.',
+)
+parser.add_argument('--mc', type=int, default=PAPER_PARAMS['monte_carlo'])
 args = parser.parse_args()
 
-cfg = SimulationConfig(monte_carlo=args.mc)
+params = dict(PAPER_PARAMS)
+params['monte_carlo'] = args.mc
 
-sim = MonteCarloSimulation(cfg)
-df = sim.run()
+simulation = PaperSimulation(params)
+df = simulation.run()
 
 plot_system_model('figures/figure1_system_model.png')
 df.to_csv('results/figure2_results.csv', index=False)
 plot_capacity(df, 'figures/figure2_ergodic_capacity.png')
 
-print('Finished generating all figures.')
+print('Finished generating all paper figures and numerical results.')
