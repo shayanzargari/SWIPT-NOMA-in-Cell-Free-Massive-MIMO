1017e085cd3fe48e0bd83f582047bca1d7f3af67
Shayan Zargari <104786563+shayanzargari@users.noreply.github.com>
2026-05-20 13:39:02 -0400

    Make simulation smoke tests lightweight

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,6 +8,8 @@ def small_params():
     params['num_users_min'] = 2
     params['num_users_max'] = 6
     params['user_step'] = 2
+    params['expectation_samples'] = 2
+    params['figure4_users'] = 4
     return params
 
 
@@ -26,9 +28,7 @@ def test_figure3_full_simulation_runs():
 
 
 def test_figure4_full_simulation_runs():
-    params = small_params()
-    params['monte_carlo'] = 1
-    df = figure4_curves(params)
+    df = figure4_curves(small_params())
     assert len(df) > 0
     assert {'power_splitting_ratio', 'rho_1', 'rho_085'}.issubset(df.columns)
     assert df.notna().all().all()
